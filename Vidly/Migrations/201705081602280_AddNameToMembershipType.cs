83cce8f74dc2f16b5e7315a2a989def7cb0f7da0
reilly472 <reilly@whitewavemedia.com>
2017-05-08 15:24:38 -0400

    finished section 5

---
+++ Vidly/Migrations/201705081602280_AddNameToMembershipType.cs
@@ -0,0 +1,59 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddNameToMembershipType : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Movies",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 255),
+                        GenreId = c.Byte(nullable: false),
+                        DateAdded = c.DateTime(nullable: false),
+                        ReleaseDate = c.DateTime(nullable: false),
+                        NumberInStock = c.Byte(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Genres", t => t.GenreId, cascadeDelete: true)
+                .Index(t => t.GenreId);
+            
+            CreateTable(
+                "dbo.Genres",
+                c => new
+                    {
+                        Id = c.Byte(nullable: false),
+                        Name = c.String(nullable: false, maxLength: 255),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            AddColumn("dbo.Customers", "Birthdate", c => c.DateTime());
+            AddColumn("dbo.MembershipTypes", "Name", c => c.String(nullable: false));
+
+            Sql("INSERT INTO Genres (Id, Name) VALUES (1, 'Action')");
+            Sql("INSERT INTO Genres (Id, Name) VALUES (2, 'Thriller')");
+            Sql("INSERT INTO Genres (Id, Name) VALUES (3, 'Family')");
+            Sql("INSERT INTO Genres (Id, Name) VALUES (4, 'Romance')");
+            Sql("INSERT INTO Genres (Id, Name) VALUES (5, 'Comedy')");
+
+            Sql("UPDATE MembershipTypes SET Name = 'Pay as You Go' WHERE Id = 1");
+            Sql("UPDATE MembershipTypes SET Name = 'Monthly' WHERE Id = 2");
+            Sql("UPDATE MembershipTypes SET Name = 'Quarterly' WHERE Id = 3");
+            Sql("UPDATE MembershipTypes SET Name = 'Annual' WHERE Id = 4");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Movies", "GenreId", "dbo.Genres");
+            DropIndex("dbo.Movies", new[] { "GenreId" });
+            DropColumn("dbo.MembershipTypes", "Name");
+            DropColumn("dbo.Customers", "Birthdate");
+            DropTable("dbo.Genres");
+            DropTable("dbo.Movies");
+        }
+    }
+}
